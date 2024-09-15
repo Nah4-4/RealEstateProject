8647aedc69b1e976ef5de5cd09c8b9e45f50ce58
Nahom <nahokifl@gmail.com>
2024-09-15 20:43:00 +0300

    modify

---
+++ RealEstateProject/House.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateProject
+{       
+    public class House
+    {
+        public string ImagePath { get; set; }
+        public string HouseId { get; set; }
+    }
+}
--- RealEstateProject/view/MainWindow.xaml.cs
+++ RealEstateProject/view/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -16,9 +18,26 @@ namespace RealEstateProject.view
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<House> Houses { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Houses = new ObservableCollection<House>
+            {
+                new House { ImagePath = "/Images/whouse.jpg", HouseId = "House1" },
+                new House { ImagePath = "/Images/images.jpg", HouseId = "House1" },
+                new House { ImagePath = "/Images/whouse.jpg", HouseId = "House1" },
+
+            };
+
+            // Set DataContext to bind data to the UI
+            DataContext = this;
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("11");
         }
 
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
@@ -39,7 +58,11 @@ namespace RealEstateProject.view
 
         private void btnMaximize_Click(object sender, RoutedEventArgs e)
         {
-            WindowState = WindowState.Maximized;
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState=WindowState.Maximized;
         }
+
     }
 }
